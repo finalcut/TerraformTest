1d31273d5a061d616844f8cfd5896f6dcb2d94bc
Rawlinson <Bill.Rawlinson@uspsector.com>
2019-02-20 08:31:35 -0500

    switch image to centos on windows

--- virtualbox.tf
+++ virtualbox.tf
@@ -1,7 +1,7 @@
 resource "virtualbox_vm" "node" {
     count = 1
     name = "${var.vm_name}"
-    url = "https://vagrantcloud.com/ubuntu/boxes/xenial64/versions/20180420.0.0/providers/virtualbox.box"
+    url = "https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vagrant-1601_01.VirtualBox.box"
     image = "./virtualbox.box"
     cpus = 2
     memory = "512 mib",
