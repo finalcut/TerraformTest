951a6c22058c0cb1803baa3c19414d4493b1caa3
Bill Rawlinson <bill.rawlinson@uspsector.com>
2019-02-22 15:34:13 -0500

    enable terraform

--- amazon.bat
+++ amazon.bat
@@ -2,18 +2,18 @@ IF %useProxy% == true (goto :proxy)
 
 :terraform
 echo "Start Terraform Steps"
-REM terraform init
-REM terraform plan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "access_key=%awsAccessKey%" -var "secret_key=%awsSecretKey%" -out myPlan
-REM terraform apply -input=false myPlan
+terraform init
+terraform plan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "access_key=%awsAccessKey%" -var "secret_key=%awsSecretKey%" -out myPlan
+terraform apply -input=false myPlan
 echo "End Terraform Steps"
 goto :eof
 
 :proxy
-echo "Set the Proxy"
+echo "Set the Proxies"
 set HTTP_PROXY=http://proxy.hud.gov:8080
 set HTTPS_PROXY=http://proxy.hud.gov:8443
 set SOCKS_PROXY=http://proxy.hud.gov:5050
-echo %useProxy%
+echo "Proxies are Set"
 goto :terraform
 
 :eof
