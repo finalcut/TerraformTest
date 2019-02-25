c0996b59d751ebd0b78d9dce72d8b96a6889123f
Bill Rawlinson <bill.rawlinson@uspsector.com>
2019-02-25 09:38:20 -0500

    move proxy to its own batch

--- azure.bat
+++ azure.bat
@@ -2,19 +2,15 @@ IF %useProxy% == true (goto :proxy)
 
 :terraform
 echo "Start Terraform Steps"
-REM terraform init
-REM terraform plan -out myPlan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "client_secret=%azureSecretKey%" -var "tenant_id=%azureTennantId%" -var "subscription_id=%azureSubscriptionId%" -var "client_id=%azureApplicationId%" -var "vm_password=%vm_password%"
-REM terraform apply -input=false myPlan
+terraform init
+terraform plan -out myPlan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "client_secret=%azureSecretKey%" -var "tenant_id=%azureTennantId%" -var "subscription_id=%azureSubscriptionId%" -var "client_id=%azureApplicationId%" -var "vm_password=%vm_password%"
+terraform apply -input=false myPlan
 echo "End Terraform Steps"
 goto :eof
 
 :proxy
 echo "Set the Proxy"
-set HTTP_PROXY=http://proxy.hud.gov:8080
-set HTTPS_PROXY=http://proxy.hud.gov:8443
-set SOCKS_PROXY=http://proxy.hud.gov:5050
-
-echo %useProxy%
+call proxy.bat
 goto :terraform
 
 :eof
