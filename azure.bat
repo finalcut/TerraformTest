09d1a39ed33dc45739da6ce0d13a2e1c5ae045de
Bill Rawlinson <bill.rawlinson@uspsector.com>
2019-03-05 08:42:56 -0500

    simplify bat; remove vars

--- azure.bat
+++ azure.bat
@@ -3,7 +3,7 @@ IF %useProxy% == true (goto :proxy)
 :terraform
 echo "Start Terraform Steps"
 terraform init
-terraform plan -out myPlan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "client_secret=%azureSecretKey%" -var "tenant_id=%azureTennantId%" -var "subscription_id=%azureSubscriptionId%" -var "client_id=%azureApplicationId%" -var "vm_password=%vm_password%"
+terraform plan -out myPlan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "client_secret=%azureSecretKey%" -var "vm_password=%vm_password%"
 terraform apply -input=false myPlan
 echo "End Terraform Steps"
 goto :eof
