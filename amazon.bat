370b323fb9fd43d3409a893d29a1559e62c78623
Bill Rawlinson <bill.rawlinson@uspsector.com>
2019-03-05 08:56:37 -0500

    dont pass access key via args

--- amazon.bat
+++ amazon.bat
@@ -3,7 +3,7 @@ IF %useProxy% == true (goto :proxy)
 :terraform
 echo "Start Terraform Steps"
 terraform init
-terraform plan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "access_key=%awsAccessKey%" -var "secret_key=%awsSecretKey%" -out %vmID%
+terraform plan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "secret_key=%awsSecretKey%" -out %vmID%
 terraform apply -input=false %vmID%
 echo "End Terraform Steps"
 goto :eof
