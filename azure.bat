d88d65f9fcdbd28d70fc08f95d1528f33e916f33
Bill Rawlinson <bill.rawlinson@uspsector.com>
2019-03-05 08:57:26 -0500

    use id as plan name

--- azure.bat
+++ azure.bat
@@ -3,8 +3,8 @@ IF %useProxy% == true (goto :proxy)
 :terraform
 echo "Start Terraform Steps"
 terraform init
-terraform plan -out myPlan -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "client_secret=%azureSecretKey%" -var "vm_password=%vm_password%"
-terraform apply -input=false myPlan
+terraform plan -out %vmID% -var "vm_id=%vmId%" -var "vm_name=%vmName%" -var "client_secret=%azureSecretKey%" -var "vm_password=%vm_password%"
+terraform apply -input=false %vmID%
 echo "End Terraform Steps"
 goto :eof
 
